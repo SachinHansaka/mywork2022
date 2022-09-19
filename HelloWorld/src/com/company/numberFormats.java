a486e11b4d30a59c93809d22047df9f98aac8cf5
SachinHansaka <sachin.20210219@iit.ac.lk>
2022-09-20 02:23:57 +0530

    new one 3

--- HelloWorld/src/com/company/numberFormats.java
+++ HelloWorld/src/com/company/numberFormats.java
@@ -1,5 +1,29 @@
 package com.company;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class numberFormats {
+
+    public static void main(String[] args) {
+
+        var doubleValue = 10000000.53;
+
+        var numF = NumberFormat.getNumberInstance();
+        System.out.println("Number : " + numF.format(doubleValue));
+
+        var intF = NumberFormat.getIntegerInstance();
+        System.out.println("Number : " + intF.format(doubleValue));
+
+        intF.setGroupingUsed(false);
+        System.out.println("Number : " + intF.format(doubleValue));
+
+        var locale = new Locale("de", "DE");
+        var localeFormatter = NumberFormat.getNumberInstance(locale);
+        System.out.println("Number : " + localeFormatter.format(doubleValue));
+
+        var currencyFormatter = NumberFormat.getCurrencyInstance();
+        System.out.println(currencyFormatter.format(doubleValue));
+    }
 
 }
